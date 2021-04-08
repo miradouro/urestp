94216c1b1cba272635db64bca38c94677d80fd28
Miradouro <rafaelmiradouro@gmail.com>
2021-04-08 06:06:24 -0300

    adicionado serializer/viewset/rota

---
+++ books/api/viewsets.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from books.api.serializers import BooksSerializer
+from books.models import Books
+
+
+class BooksViewset(viewsets.ModelViewSet):
+    serializer_class = BooksSerializer
+    queryset = Books.objects.all()
+
